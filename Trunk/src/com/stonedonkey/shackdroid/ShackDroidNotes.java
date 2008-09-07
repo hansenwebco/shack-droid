625b0c9b894afc5bf739649be76627fa45fcb428
stonedonkey <stonedonkey@gmail.com>
2008-09-07 03:40:19 +0000

    Added notes Activity and related XML layouts.

---
+++ Trunk/src/com/stonedonkey/shackdroid/ShackDroidNotes.java
@@ -0,0 +1,16 @@
+package com.stonedonkey.shackdroid;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class ShackDroidNotes extends ListActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.notes);
+	}
+
+}
