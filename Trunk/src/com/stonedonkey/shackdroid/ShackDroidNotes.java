74ae62bfc42c6c003d1f0c4e2f23546dd027e61f
stonedonkey <stonedonkey@gmail.com>
2008-09-09 02:58:02 +0000

    context menu for ShackNotes

--- Trunk/src/com/stonedonkey/shackdroid/ShackDroidNotes.java
+++ Trunk/src/com/stonedonkey/shackdroid/ShackDroidNotes.java
@@ -4,7 +4,11 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.View.OnCreateContextMenuListener;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class ShackDroidNotes extends ListActivity {
@@ -29,22 +33,57 @@ public class ShackDroidNotes extends ListActivity {
 				notesCursor);
 		setListAdapter(nva);
 
+		nm.close();
+
+		// add a listener to the default view to handle holding down
+		// on the screen to view or delete.. default cilck is still view.
+		getListView().setOnCreateContextMenuListener(
+				new OnCreateContextMenuListener() {
+					@Override
+					public void onCreateContextMenu(ContextMenu menu, View v,
+							ContextMenuInfo menuInfo) {
+						menu.add(0, 0, 0, "View ShackMark");
+						menu.add(0, 0, 0, "Delete ShackMark");
+					}
+				});
+		// Add click action
+		getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+					@Override
+					public void onItemClick(AdapterView<?> parent, View v,	int position, long id) {
+						// Clicking an item starts editing it
+						
+							DeleteShackMark(id);
+							
+						
+					}
+
+				});
+
 	}
+
+	private void DeleteShackMark(Long deleteID)
+	{
+		
+	}
+	
 	@Override
 	public void onListItemClick(ListView l, View v, int position, long id) {
 		super.onListItemClick(l, v, position, id);
-		
+
 		notesCursor.moveToPosition(position);
-		
-		String postID =  notesCursor.getString(notesCursor.getColumnIndexOrThrow("threadID"));
-		String storyID = notesCursor.getString(notesCursor.getColumnIndexOrThrow("storyID"));
-		
+
+		String postID = notesCursor.getString(notesCursor
+				.getColumnIndexOrThrow("threadID"));
+		String storyID = notesCursor.getString(notesCursor
+				.getColumnIndexOrThrow("storyID"));
+
 		Intent intent = new Intent();
 		intent.setClass(this, ShackDroidThread.class);
 		intent.putExtra("postID", postID); // the value must be a string
 		intent.putExtra("storyID", storyID);
 		startActivity(intent);
-	
+
 	}
 
 }
