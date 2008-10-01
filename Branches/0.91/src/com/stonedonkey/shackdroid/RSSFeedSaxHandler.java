15d5d69d27497193a18118f7e86c198d0d8caec3
stonedonkey <stonedonkey@19a154ba-9bc5-11dd-ada3-7767e702ac26>
2008-10-01 20:41:36 +0000

    Massive renaming of .java files incoming.. save me obiwon!

---
+++ Branches/0.91/src/com/stonedonkey/shackdroid/RSSFeedSaxHandler.java
@@ -0,0 +1,105 @@
+package com.stonedonkey.shackdroid;
+
+import java.util.ArrayList;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import android.content.Context;
+
+public class RSSFeedSaxHandler extends DefaultHandler {
+
+	@SuppressWarnings("unused")
+	private Context context = null;
+		
+	private ArrayList<ShackRSS> rssItems = new ArrayList<ShackRSS>();
+	
+	private Boolean item = false;
+	private Boolean title = false;
+	private Boolean description = false;
+	private Boolean link = false;
+	private Boolean datePosted = false;
+
+	private String titleText = "";
+	private String descriptionText ="";
+	private String linkText = "";
+	private String datePostedText = "";
+
+	
+	public RSSFeedSaxHandler(Context context) {
+		this.context =context;
+	}
+
+	public ArrayList<ShackRSS> getRssItems() {
+		return rssItems;
+	}
+
+	@Override
+	public void startElement(final String nsURL, final String localName, final String rawName,final Attributes attributes) throws SAXException
+	{
+		if ("item".equalsIgnoreCase(localName))
+			this.item = true;
+		
+		if ("title".equalsIgnoreCase(localName))
+			this.title = true;
+		
+		if ("description".equalsIgnoreCase(localName))
+			this.description = true;
+
+		if ("link".equalsIgnoreCase(localName))
+			this.link = true;
+		
+		if ("date".equalsIgnoreCase(localName))
+			this.datePosted = true;
+				
+		
+	}
+	@Override
+	public void endElement(final String nsURI, final String localName,final String rawName)throws SAXException 
+	{
+		
+		if ("title".equalsIgnoreCase(localName))
+			this.title = false;
+		
+		if ("description".equalsIgnoreCase(localName))
+			this.description = false;
+
+		if ("link".equalsIgnoreCase(localName))
+			this.link = false;
+		
+		if ("date".equalsIgnoreCase(localName))
+			this.datePosted = false;
+		
+		if ("item".equalsIgnoreCase(localName))
+		{
+			this.item = false;
+			
+			ShackRSS rssItem = new ShackRSS(titleText, descriptionText, linkText, datePostedText);
+			rssItems.add(rssItem);
+			
+			titleText="";
+			descriptionText="";
+			linkText="";
+			datePostedText="";
+		}
+		
+		
+	}
+	@Override
+	public void characters(char[] ch, int start, int length) throws SAXException 
+	{
+		if (title && item)
+			titleText = titleText +  new String(ch,start,length);
+		
+		if (description && item)
+			descriptionText = descriptionText +  new String(ch,start,length);
+		
+		if (link && item)
+			 linkText = linkText + new String(ch,start,length);
+		
+		if (datePosted && item)
+			datePostedText = datePostedText +  new String(ch,start,length);
+		
+	}
+}
---
+++ Branches/0.91/src/com/stonedonkey/shackdroid/ShackDroidWebView.java
@@ -0,0 +1,33 @@
+package com.stonedonkey.shackdroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class ShackDroidWebView extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		Bundle extras = this.getIntent().getExtras();
+		String storyURL = extras.getString("URL");
+		String storyTitle = extras.getString("Title");
+				
+		setContentView(R.layout.webview);
+		this.setTitle(storyTitle);
+		
+		WebView wv = (WebView)findViewById(R.id.WebViewShackDroid);
+		wv.getSettings().setJavaScriptEnabled(true);
+		wv.getSettings().setPluginsEnabled(true);
+		wv.getSettings().setSavePassword(true);
+		wv.getSettings().setSupportZoom(true);
+
+		
+		wv.loadUrl(storyURL);
+		
+		super.onCreate(savedInstanceState);
+	}
+
+	
+	
+}
