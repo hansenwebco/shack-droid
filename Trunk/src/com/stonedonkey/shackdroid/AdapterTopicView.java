ae28a63027520ddaad5239778ef17cf2a3ef85e0
stonedonkey <stonedonkey@19a154ba-9bc5-11dd-ada3-7767e702ac26>
2009-11-19 04:24:48 +0000

    Now checks for username, still need to add prefs.

--- Trunk/src/com/stonedonkey/shackdroid/AdapterTopicView.java
+++ Trunk/src/com/stonedonkey/shackdroid/AdapterTopicView.java
@@ -12,7 +12,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class AdapterTopicView extends BaseAdapter {
