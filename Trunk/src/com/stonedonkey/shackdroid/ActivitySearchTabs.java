9b2c0d5a182d359142b943f9819653b1bb37cff4
stonedonkey <stonedonkey@gmail.com>
2009-11-19 19:13:18 +0000

    Changed tab name for Search

--- Trunk/src/com/stonedonkey/shackdroid/ActivitySearchTabs.java
+++ Trunk/src/com/stonedonkey/shackdroid/ActivitySearchTabs.java
@@ -2,9 +2,7 @@ package com.stonedonkey.shackdroid;
 
 import android.app.TabActivity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.widget.TabHost;
 
 
@@ -18,19 +16,17 @@ public class ActivitySearchTabs extends TabActivity {
 	Helper.SetWindowState(getWindow(),this);
 		
 	final TabHost tabHost = getTabHost();
-	tabHost.addTab(tabHost.newTabSpec("Search").setIndicator("Local").setContent(new Intent(this,ActivitySearch.class).putExtra("view", "search")));
+	tabHost.addTab(tabHost.newTabSpec("Search").setIndicator("Search").setContent(new Intent(this,ActivitySearch.class).putExtra("view", "search")));
 	tabHost.addTab(tabHost.newTabSpec("Vanity").setIndicator("Vanity").setContent(new Intent(this,ActivitySearch.class).putExtra("view", "vanity")));
 	tabHost.addTab(tabHost.newTabSpec("Parent").setIndicator("Parent").setContent(new Intent(this,ActivitySearch.class).putExtra("view", "parent")));
 	tabHost.addTab(tabHost.newTabSpec("Yours").setIndicator("Yours").setContent(new Intent(this,ActivitySearch.class).putExtra("view", "yours")));
 	
-	SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-	String username = prefs.getString("shackLogin", "");
+	//int tab = getTabHost().getCurrentTab();
 	
-	if (username ==null || username.length() == 0) {
-		//int test =tabHost.getChildCount();
-		//tabHost.getChildAt(1).setEnabled(false);
-		
-	}
+	//SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+	//String username = prefs.getString("shackLogin", "");
+	
+	
 	
 	}	
 }
