9416b915d8e440cb54f4491a6234c0caf870b478
stonedonkey <stonedonkey@gmail.com>
2009-11-20 17:41:14 +0000

    Tagging 1.62

---
+++ Tags/1.62/src/com/stonedonkey/shackdroid/ActivityPreferences.java
@@ -0,0 +1,19 @@
+package com.stonedonkey.shackdroid;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class ActivityPreferences extends PreferenceActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+	
+		Helper.SetWindowState(getWindow(),this);
+		     
+		super.onCreate(savedInstanceState);
+		
+		addPreferencesFromResource(R.xml.preferences);
+		
+	}
+
+}
