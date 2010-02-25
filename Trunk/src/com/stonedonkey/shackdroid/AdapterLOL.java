04f79ece87fbd7e43a58bdad6300821f0740331f
stonedonkey <stonedonkey@19a154ba-9bc5-11dd-ada3-7767e702ac26>
2010-02-25 16:28:42 +0000

    Continued cleanup of LOL layout. Added function to parse LOL dates to helper. (really need to clean this up one day)

--- Trunk/src/com/stonedonkey/shackdroid/AdapterLOL.java
+++ Trunk/src/com/stonedonkey/shackdroid/AdapterLOL.java
@@ -3,6 +3,7 @@ package com.stonedonkey.shackdroid;
 import java.util.ArrayList;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,12 +15,16 @@ public class AdapterLOL extends BaseAdapter {
 	private ArrayList<ShackLOL> posts;
 	private Context context;
 	private int rowResouceID;
+	static Typeface face;
 	
 	public AdapterLOL(Context context, ArrayList<ShackLOL> posts,int rowResouceID)
 	{
 		this.posts = posts;
 		this.context = context;
 		this.rowResouceID = rowResouceID;
+		
+		 face = Typeface.createFromAsset(context.getAssets(), "fonts/arial.ttf");
+		
 	}
 	
 	@Override
@@ -50,13 +55,32 @@ public class AdapterLOL extends BaseAdapter {
 		LayoutInflater inflate = LayoutInflater.from(context);
 		View v = inflate.inflate(rowResouceID,parent,false);
 						
-		
+		// format and set post text
 		String postBody = post.getBody();
-		postBody= postBody.replaceAll("</?\\w++[^>]*+>", ""); // remove HTML tags
-		postBody = postBody.replaceAll("(\r\n|\r|\n|\n\r)", "");
+		postBody = postBody.replaceAll("</?\\w++[^>]*+>", ""); // remove HTML tags
+		//postBody = postBody.replaceAll("(\r\n|\r|\n|\n\r)", "");
+		
+		if (postBody.length() > 255)
+			postBody = postBody.substring(0,255);
 		
 		TextView postText = (TextView)v.findViewById(R.id.TextViewLolPostText);
 		postText.setText(postBody);
+		postText.setTypeface(face);
+		
+		// set poster name
+		TextView posterName = (TextView)v.findViewById(R.id.TextViewLolPosterName);
+		posterName.setText(post.getAuthor());
+		posterName.setTypeface(face);
+		
+		// Tag count
+		TextView tagCount = (TextView)v.findViewById(R.id.TextViewLolTagCount);
+		tagCount.setText(post.getTagCount() + "\n" + post.getTag().toUpperCase() + "s");
+		tagCount.setTypeface(face);
+		
+		 // date posted
+		TextView datePosted = (TextView)v.findViewById(R.id.TextViewLOLDatePosted);
+		datePosted.setText(Helper.FormatLOLDate(post.getCommentDate()));
+		datePosted.setTypeface(face);
 		
 		return v;
 	}
