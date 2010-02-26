d738de7c4ba71dcbaa791a6ef8567f3ffcfff5a3
stonedonkey <stonedonkey@gmail.com>
2010-02-26 21:50:23 +0000

    Creating 1.64 branch

---
+++ Tags/1.64/src/com/stonedonkey/shackdroid/ShackRSS.java
@@ -0,0 +1,29 @@
+package com.stonedonkey.shackdroid;
+
+public final class ShackRSS {
+
+	private String title;
+	private String description;
+	private String link;
+	private String datePosted;
+	
+	public ShackRSS(String title, String description, String link, String datePosted)
+	{
+		this.title = title;
+		this.description  = description;
+		this.link = link;
+		this.datePosted = datePosted;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public  String getDescription() {
+		return description;
+	}
+	public  String getLink() {
+		return link;
+	}
+	public  String getDatePosted() {
+		return datePosted;
+	}
+}
