5555a5e18e9f2d81f7d2da17449ccb7f10f56fc4
stonedonkey <stonedonkey@gmail.com>
2010-04-06 19:26:49 +0000

    Should now be calling the background service no matter how the app is started. Can't seem to get it to not reset the service on start of app, hence every time the Splash screen fires, the app checks for new messages, not the end of the world, but not great.

--- Trunk/src/com/stonedonkey/shackdroid/ShackDroidServicesReceiver.java
+++ Trunk/src/com/stonedonkey/shackdroid/ShackDroidServicesReceiver.java
@@ -9,9 +9,10 @@ public class ShackDroidServicesReceiver extends BroadcastReceiver
 {
 	@Override
 	public void onReceive(Context context, Intent arg1) {
-		Log.d(this.toString(), "SM Check broadcast received.");
+		Log.d("ShackDroid", "SM Check broadcast received.");
 		
-		if (arg1 != null && arg1.getAction() == Intent.ACTION_BOOT_COMPLETED){
+		if (arg1.getAction() != null && arg1.getAction().equals(Intent.ACTION_BOOT_COMPLETED)){
+			Log.d("ShackDroid", "Detected Device Boot - Enable SMs.");
 			if (Helper.CheckAllowSMService(context)){
 				Helper.setSMAlarm(context);
 			}
@@ -20,11 +21,16 @@ public class ShackDroidServicesReceiver extends BroadcastReceiver
 			if (Helper.CheckAllowSMService(context)){
 				// Grab a wake-lock now to stop the phone from sleeping between ending this function and the service
 				// actually starting.  Hold it for 3 mins max.
+				
+				Log.d("ShackDroid", "onReceive Not booting.");
+				
 				ActivityShackDroidServices.getLock(context).acquire(180000); 
 				context.startService(new Intent(context, ActivityShackDroidServices.class));
 			}
-			else
+			else {
+				Log.d("ShackDroid", "onReceive stopping.");
 				context.stopService(new Intent(context, ActivityShackDroidServices.class));
+			}
 		}
 	}
 }
