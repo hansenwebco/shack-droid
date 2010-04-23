a80d68a38ef072952ff079b85654033005851b68
stonedonkey <stonedonkey@gmail.com>
2010-04-23 21:30:06 +0000

    Playing with some JSON Parsing..

--- Trunk/src/com/stonedonkey/shackdroid/JSONHandlerTopicView.java
+++ Trunk/src/com/stonedonkey/shackdroid/JSONHandlerTopicView.java
@@ -1,14 +1,10 @@
 package com.stonedonkey.shackdroid;
 
-import java.util.ArrayList;
+import java.net.URL;
 
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
-import org.json.JSONObject;
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.JsonParser;
+import org.codehaus.jackson.JsonToken;
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -16,7 +12,7 @@ import android.preference.PreferenceManager;
 
 public class JSONHandlerTopicView {
 
-	private ArrayList<ShackPost> posts = new ArrayList<ShackPost>();
+	//private ArrayList<ShackPost> posts = new ArrayList<ShackPost>();
 	
 	public JSONHandlerTopicView(Context context)
 	{
@@ -25,34 +21,51 @@ public class JSONHandlerTopicView {
 		
 		final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
 		final String feedURL = prefs.getString("shackFeedURL",context.getString(R.string.default_api));
-		final String url =feedURL + "/index.json";
+		final URL url = new URL(feedURL + "/index.json");
  
+		JsonFactory jsonFactory = new JsonFactory();
+		JsonParser jp = jsonFactory.createJsonParser(url);
 		
-
+		jp.nextToken();
+		while (jp.nextToken() != JsonToken.END_OBJECT)
+		{
+			final String fieldName = jp.getCurrentName();
+			if (fieldName.equalsIgnoreCase("comments"))
+			{
+				
+			}
+					
+			
+			
+		}
+		 
 		
-		// lets get the URL
-		HttpClient httpclient = new DefaultHttpClient();
-		HttpGet request = new HttpGet(url);
-		ResponseHandler handler = new BasicResponseHandler();
-		//you result will be String :
-		String result = (String) httpclient.execute(request, handler);
 		
-		//JSONObject json = (JSONObject) JSONSerializer.toJSON( jsonTxt );
-		final JSONObject json = new  JSONObject(result);
+		jp.close();
 		
-		JSONArray comments = json.getJSONArray("comments");
-		JSONObject comment = comments.getJSONObject(0);
 		
-		final String id = comment.getString("id");
-		final String category = comment.getString("category");
+//		// lets get the URL
+//		HttpClient httpclient = new DefaultHttpClient();
+//		HttpGet request = new HttpGet(url);
+//		ResponseHandler handler = new BasicResponseHandler();
+//		//you result will be String :
+//		String result = (String) httpclient.execute(request, handler);
+//		
+//		//JSONObject json = (JSONObject) JSONSerializer.toJSON( jsonTxt );
+//		final JSONObject json = new  JSONObject(result);
+//		
+//		JSONArray comments = json.getJSONArray("comments");
+//		JSONObject comment = comments.getJSONObject(0);
+//		
+//		final String id = comment.getString("id");
+//		final String category = comment.getString("category");
 		
-		int i= 1;
+		
 		
 		}
 		catch (Exception ex)
 		{
-			String fail;
-			fail = ex.getMessage();
+			
 		}
 		
 		
