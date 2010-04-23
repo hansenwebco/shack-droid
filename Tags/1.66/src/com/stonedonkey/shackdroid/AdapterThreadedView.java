55f0056cc0746e6310ef6738de76f451320b617c
stonedonkey <stonedonkey@gmail.com>
2010-04-23 20:13:04 +0000

    Tagging 1.66

---
+++ Tags/1.66/src/com/stonedonkey/shackdroid/AdapterThreadedView.java
@@ -0,0 +1,112 @@
+package com.stonedonkey.shackdroid;
+
+import java.util.List;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.preference.PreferenceManager;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class AdapterThreadedView<TopicRow> extends BaseAdapter {
+
+	//private Context context;
+	private List<ShackPost> topicList;
+	private int rowResouceID;
+	static Typeface face;
+	private int selectedRow = 0;
+	
+	private String login;
+	private Boolean highlightThread;
+	private int fontSize;
+	private TextView threadPreview;
+	private LayoutInflater inflate;
+	
+	public AdapterThreadedView(Context context,int rowResouceID, List<ShackPost> topicList,int selectedRow ){
+		//this.context = context;
+		this.topicList = topicList;
+		this.rowResouceID = rowResouceID;
+		this.selectedRow = selectedRow;
+		
+		face = Typeface.createFromAsset(context.getAssets(), "fonts/arial.ttf");
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+		login = prefs.getString("shackLogin", "");
+		highlightThread = prefs.getBoolean("highlightUserThreads", true);
+		fontSize = Integer.parseInt(prefs.getString("fontSize", "12"));
+		inflate = LayoutInflater.from(context);
+	}
+	
+	public void setSelectedRow(int selectedRow) {
+		this.selectedRow = selectedRow;
+	}
+
+	
+	@Override
+	public int getCount() {
+		return topicList.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return topicList.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		ShackPost post = topicList.get(position);
+		return Long.parseLong(post.getPostID());
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View v;
+		ShackPost post = topicList.get(position);
+		if (convertView == null){
+			v = inflate.inflate(rowResouceID,parent,false);
+		}
+		else{
+			v = convertView;
+		}
+		
+		// bind the TextViews to the items in our datasource
+		threadPreview = (TextView)v.findViewById(R.id.TextViewThreadPreview);
+				
+		if (threadPreview != null)
+		{
+			threadPreview.setTypeface(face);
+
+			// chazums
+			// Now just moves the text box to the right instead of padding text.
+			int postIndent = 7* post.getIndent(); // avoid multiple lookups
+	
+			threadPreview.setPadding(postIndent, threadPreview.getPaddingTop(), threadPreview.getPaddingBottom(), threadPreview.getPaddingRight());
+			String postText = post.getPostPreview();
+			 
+			// show this users posts as blue
+			 if (highlightThread == true && post.getPosterName().toString().equalsIgnoreCase(login))
+					threadPreview.setTextColor(Color.parseColor("#00BFF3"));
+			 else if (post.getPostIndex() > 9)
+				threadPreview.setTextColor(Color.parseColor("#777777"));
+			else
+				threadPreview.setTextColor(Color.parseColor("#FFFFFF"));
+			
+			
+			if (position == selectedRow )
+				threadPreview.setBackgroundColor(Color.parseColor("#274FD3"));
+			else
+				threadPreview.setBackgroundColor(Color.TRANSPARENT);
+		
+			//postText = pad + postText;
+			threadPreview.setTextSize(TypedValue.COMPLEX_UNIT_SP, fontSize);
+			threadPreview.setText(postText);
+			
+		}
+		return v;
+	}
+}
