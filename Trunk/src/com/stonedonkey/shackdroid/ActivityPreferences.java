1e777a1ea6130f3b925c2ad7151ce2bafce769f9
stonedonkey <stonedonkey@19a154ba-9bc5-11dd-ada3-7767e702ac26>
2010-05-28 15:35:38 +0000

    Cleaned up some old JSON testing code. Rearranged the preferences a bit.

--- Trunk/src/com/stonedonkey/shackdroid/ActivityPreferences.java
+++ Trunk/src/com/stonedonkey/shackdroid/ActivityPreferences.java
@@ -5,7 +5,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.Uri;
 import android.os.Bundle;
