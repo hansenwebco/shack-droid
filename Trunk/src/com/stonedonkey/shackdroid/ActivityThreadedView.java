86a754e732ddb38caf5d09ebb4563b97b2251585
stonedonkey <stonedonkey@gmail.com>
2010-06-14 12:38:08 +0000

    Fixed null posts error, via Bug report tool.
    Exception class
    java.lang.IndexOutOfBoundsException
    Source method
    ArrayList.get()
    
    java.lang.IndexOutOfBoundsException: Invalid location 0, size is 0
      at java.util.ArrayList.get(ArrayList.java:341)
      at com.stonedonkey.shackdroid.ActivityThreadedView.UpdatePostText(ActivityThreadedView.java:360)
      at com.stonedonkey.shackdroid.ActivityThreadedView.ShowData(ActivityThreadedView.java:449)
      at com.stonedonkey.shackdroid.ActivityThreadedView.access$3(ActivityThreadedView.java:441)
      at com.stonedonkey.shackdroid.ActivityThreadedView$1.handleMessage(ActivityThreadedView.java:310)
      at android.os.Handler.dispatchMessage(Handler.java:99)
      at android.os.Looper.loop(Looper.java:123)
      at android.app.ActivityThread.main(ActivityThread.java:4363)
      at java.lang.reflect.Method.invokeNative(Method.java:-2)
      at java.lang.reflect.Method.invoke(Method.java:521)
      at com.android.internal.os.ZygoteInit$MethodAndArgsCaller.run(ZygoteInit.java:860)
      at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:618)
      at dalvik.system.NativeStart.main(NativeStart.java:-2)

--- Trunk/src/com/stonedonkey/shackdroid/ActivityThreadedView.java
+++ Trunk/src/com/stonedonkey/shackdroid/ActivityThreadedView.java
@@ -355,6 +355,9 @@ public class ActivityThreadedView extends ListActivity implements Runnable, Shac
 	}
 	private void UpdatePostText(int position, Boolean addSpoilerMarkers)
 	{
+		if (posts == null) // can't bind an empty list of posts
+			return; 
+		
 		final TextView tv = (TextView) findViewById(R.id.TextViewPost);
 		final String postText = ParseShackText(posts.get(position).getPostText(),addSpoilerMarkers);
 		
