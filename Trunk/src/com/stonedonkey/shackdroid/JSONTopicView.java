066a5e3f189b31fdc302ba255e17a631bb601c76
stonedonkey <stonedonkey@gmail.com>
2010-09-29 21:16:04 +0000

    JSON Testing.

--- Trunk/src/com/stonedonkey/shackdroid/JSONTopicView.java
+++ Trunk/src/com/stonedonkey/shackdroid/JSONTopicView.java
@@ -1,17 +1,25 @@
 package com.stonedonkey.shackdroid;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
 
+import org.codehaus.jackson.map.MappingJsonFactory;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.JsonParser;
 import org.codehaus.jackson.JsonToken;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 public class JSONTopicView {
 	
@@ -61,36 +69,95 @@ public class JSONTopicView {
 	
 	}
 	
-	public ArrayList<ShackPost> GetParsedPosts()
+	public ArrayList<ShackPost> GetParsedPostsAndroid(String json)
 	{
+		
+		long startTime = System.currentTimeMillis();
+		
 		try {
-
-
-			JsonFactory jsonFactory = new JsonFactory();
-
-
-			URL url= new URL("http://shackapi.stonedonkey.com/index.json");
-
-
-
-			JsonParser jp = jsonFactory.createJsonParser(HttpHelper.HttpRequestWithGzip(url.toString(),_context));
-			if (jp.nextToken() != JsonToken.START_OBJECT) {
-				throw new IOException("Expected data to start with an Object");
-			}
-
-
-			while (jp.nextToken() != JsonToken.END_OBJECT) {
-				String fieldname = jp.getCurrentName();
-				jp.nextToken(); // move to value, or START_OBJECT/START_ARRAY
-			}
+			
+			final URL url= new URL("http://shackapi.stonedonkey.com/index.json");
+			
+			// *********************************************************************************
+			// * Basic Json Parse using default engine                                         * 
+			// *********************************************************************************
+			//final InputStream json= HttpHelper.HttpRequestWithGzip(url.toString(),_context);
+			//final JSONObject jsonResponse = new JSONObject(convertStreamToString(json));
+			final JSONObject jsonResponse = new JSONObject(json);
+			String page = jsonResponse.getString("page");
+			//JSONArray comments = jsonResponse.getJSONArray("comments");
+			//JSONObject comment = comments.getJSONObject(1);
+			//String preview = comment.getString("preview");
 
 		}
 		catch (Exception ex)
 		{
-
+			Log.e("ShackDroid","Error Parsing JSON");
 		}
+		
+		long endTime = System.currentTimeMillis();
+		long runTime =endTime - startTime;
+		Log.i("ShackDroid", "GetParsedPostsAndroid Time: " + runTime );
+		
+		return this.posts;
+	}
+	public ArrayList<ShackPost> GetParsedPostsJson(String json)
+	{
+		// *********************************************************************************
+		// * Jackson Tree Model Parser                                                     * 
+		// *********************************************************************************		
+		
+		long startTime = System.currentTimeMillis();
+		
+		try {
+			final URL url= new URL("http://shackapi.stonedonkey.com/index.json");
+
+			ObjectMapper m = new ObjectMapper();
+			//InputStream stream = HttpHelper.HttpRequestWithGzip(url.toString(), _context);
+			//JsonNode rootNode = m.readTree(stream);
+			JsonNode rootNode = m.readTree(json);
+			//String page = rootNode.path("page").getTextValue();
+			//JsonNode comments = rootNode.path("comments");
+			//String preview = comments.get(0).path("preview").getTextValue();
+		}
+		catch (Exception ex)
+		{
+			Log.e("ShackDroid","Error Parsing JSON");
+		}
+		
+		long endTime = System.currentTimeMillis();
+		long runTime =endTime - startTime;
+		Log.i("ShackDroid", "GetParsedPostsJSON Time: " + runTime );
+		
 		return this.posts;
 	}
 	
+	private static String convertStreamToString(InputStream is) {
+        /*
+         * To convert the InputStream to String we use the BufferedReader.readLine()
+         * method. We iterate until the BufferedReader return null which means
+         * there's no more data to read. Each line will appended to a StringBuilder
+         * and returned as String.
+         */
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+    }
+
 	
 }
