5f70053a4598cf7362f586f62cc0a2dec58f8f39
stonedonkey@gmail.com <stonedonkey@gmail.com@19a154ba-9bc5-11dd-ada3-7767e702ac26>
2012-07-11 18:14:09 +0000

    More cleanup. Going Limeriffic!

---
+++ Trunk/src/com/stonedonkey/shackdroid/ActivityLimeriffic.java
@@ -0,0 +1,22 @@
+package com.stonedonkey.shackdroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityLimeriffic extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		// get the list of topics
+		
+		// 
+		
+		
+	}
+
+	
+	
+}
