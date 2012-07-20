36df15ab6f23a025c63560f3980d35cba9faa0db
stonedonkey@gmail.com <stonedonkey@gmail.com@19a154ba-9bc5-11dd-ada3-7767e702ac26>
2012-07-20 15:10:40 +0000

    Removed ActionBar library and added Google v4 support.

--- Trunk/src/com/stonedonkey/shackdroid/FragmentActivityTopic.java
+++ Trunk/src/com/stonedonkey/shackdroid/FragmentActivityTopic.java
@@ -3,20 +3,17 @@ package com.stonedonkey.shackdroid;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
-
-
-
-
 public class FragmentActivityTopic extends FragmentActivity {
 
 	@Override
-	protected void onCreate(Bundle arg) {
+	protected void onCreate(Bundle arg0) {
 		// TODO Auto-generated method stub
-		super.onCreate(arg);
+		super.onCreate(arg0);
 		
 		setContentView(R.layout.mixed);
-		
-		
 	}
 
+
+	
+	
 }
